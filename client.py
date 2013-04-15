eeb3486640a1a4acc25a90017174957972eaa48e
Jordan Poles <jpoles1@gmail.com>
2013-04-15 16:27:05 -0400

    Chat Server Online
    Displays log on server, not on client

--- client.py
+++ client.py
@@ -1,9 +1,19 @@
 import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 8080                # Reserve a port for your service.
-
-s.connect((host, port))
-print s.recv(1024)
-s.close                     # Close the socket when done
+import time
+while 1:
+    mes = raw_input("Message:")
+    s = socket.socket()         # Create a socket object
+    host = "192.168.1.123" # Get local machine name
+    port = 8080                # Reserve a port for your service.
+    x=0
+    s.connect((host, port))
+    mes = raw_input("Message:")
+    s.send(mes)
+    while x==1:
+        data = s.recv(1024)
+        if data == "CLOSECONNECTION":
+            x=1
+        else:
+            print data
+    time.sleep(.1)
+    s.close                     # Close the socket when done
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import socket
+import time
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 8080                # Reserve a port for your service.
+host = "192.168.1.123" # Get local machine name
+port = 8080
+rsize = 1024                # Reserve a port for your service.
 s.bind((host, port))
-s.listen(5)                 # Now wait for client connection.
+s.listen(5)
+log = []                 # Now wait for client connection.
 while True:
-    c, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    c.send('Thank you for connecting')
-    c.close()                # Close the connection
+    c, addr = s.accept()
+    data = c.recv(rsize)
+    print(addr[0]+": "+data)
+    log.append([addr[0], data])
+    for i in log:
+        c.send(i[0]+": "+i[1])
+    c.send("CLOSECONNECTION")
+time.sleep(.1)
+c.close()                # Close the connection
